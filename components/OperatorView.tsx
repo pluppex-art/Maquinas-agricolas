90497215ba69dfdb42272fc239c3c5700f3d0c5e
pluppex-art <pluppex@gmail.com>
2026-01-02 14:47:29 -0300

    feat: Improve photo upload UX
    Removes `capture="environment"` from file inputs to allow users to select images from their gallery instead of forcing camera use. Updates button labels for clarity.

--- components/OperatorView.tsx
+++ components/OperatorView.tsx
@@ -203,7 +203,6 @@ const OperatorView: React.FC<OperatorViewProps> = ({ operator, tractors, service
                     <input 
                       type="file" 
                       accept="image/*" 
-                      capture="environment" 
                       className="hidden" 
                       ref={fileInputStart} 
                       onChange={(e) => handleFileChange(e, setStartPhoto)}
@@ -225,7 +224,7 @@ const OperatorView: React.FC<OperatorViewProps> = ({ operator, tractors, service
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mb-1" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
                           </svg>
-                          <span className="text-[10px]">Foto Inicial</span>
+                          <span className="text-[10px]">Adicionar Foto</span>
                         </>
                       )}
                     </button>
@@ -257,7 +256,6 @@ const OperatorView: React.FC<OperatorViewProps> = ({ operator, tractors, service
                     <input 
                       type="file" 
                       accept="image/*" 
-                      capture="environment" 
                       className="hidden" 
                       ref={fileInputEnd} 
                       onChange={(e) => handleFileChange(e, setEndPhoto)}
@@ -279,7 +277,7 @@ const OperatorView: React.FC<OperatorViewProps> = ({ operator, tractors, service
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mb-1" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
                           </svg>
-                          <span className="text-[10px]">Foto Final</span>
+                          <span className="text-[10px]">Adicionar Foto</span>
                         </>
                       )}
                     </button>
